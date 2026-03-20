680e4376169374540f428b29ae543e56a53f2ae6
shimzahmed <shimz.ahmed@gmail.com>
2026-03-20 11:29:24 +0500

    abc

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 var msg = 'Hello World';
 console.log(msg);
+
+function greetEveryone() {
+console.log('Hello Everyone!');
+}
+function greet(){
+console.log('Rise and shine, Contractors!');
+
+}
+
+greetEveryone();
+greet();
+
